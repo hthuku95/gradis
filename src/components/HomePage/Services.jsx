9eca172fab2f5bbfb280da69f521c23ee57afe29
HARRY OMOLLOH <hthuku95@gmail.com>
2023-08-02 15:43:23 +0300

    Created Services Component

---
+++ src/components/HomePage/Services.jsx
@@ -0,0 +1,11 @@
+import { Box,Container } from "@mui/material";
+
+const Services = () => {
+    return (
+        <Container id='section3'>
+            <Box> The services we offer</Box>
+        </Container>
+    )
+}
+
+export default Services;
