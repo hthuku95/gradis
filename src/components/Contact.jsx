dab0c1a5df1b463aaa76c5913acef045dbd070b4
HARRY OMOLLOH <hthuku95@gmail.com>
2023-08-02 15:41:29 +0300

    Created contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import { Box,Container } from "@mui/material";
+
+const Contact = () => {
+    return (
+    <Container id='section4'>
+        <Box>Contact us</Box>
+    </Container>
+    )
+}
+export default Contact;
