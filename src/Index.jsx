54553748a82e189e5d5e281640cae0656d166fc6
HARRY OMOLLOH <hthuku95@gmail.com>
2023-08-02 15:41:03 +0300

    Created index Component

---
+++ src/Index.jsx
@@ -0,0 +1,13 @@
+import {Box} from '@mui/material';
+import Home from './pages/Home';
+
+const Index = ()=>{
+
+    return (
+        <Box sx={{backgroundColor:"custom.grey"}}>
+            <Home/>
+        </Box>
+    )
+}
+
+export default Index;
