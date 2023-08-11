40b6d000e3b8ad1984f9f23ba4f252f55544973a
HARRY OMOLLOH <hthuku95@gmail.com>
2023-08-12 00:08:03 +0300

    Added Grid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Box,Container,Typography } from "@mui/material";
+import { Box,Container,Typography,Grid } from "@mui/material";
 
 const Contact = () => {
     return (
@@ -7,17 +7,47 @@ const Contact = () => {
         color:'custom.white'
     }}>
         <Container>
-            <Box sx={{
-                display:'flex',
-                justifyContent:'center'
-            }}>
-                <Typography variant="h3" sx={{
-                    mb:4,
-                    mt:8
-                }}>
-                    GRADIS LIMITED
-                </Typography>
-            </Box>
+            <Grid container spacing={4} columns={18}>
+                <Grid item xs={18} sm={18} md={6} lg={6} xl={6}>
+                    <Box sx={{
+                        display:'flex',
+                        justifyContent:'center'
+                    }}>
+                        <Typography variant="h4" sx={{
+                            mb:4,
+                            mt:8
+                        }}>
+                            GRADIS LIMITED
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={18} sm={18} md={6} lg={6} xl={6}>
+                    <Box sx={{
+                            display:'flex',
+                            justifyContent:'center'
+                        }}>
+                            <Typography variant="h4" sx={{
+                                mb:4,
+                                mt:8
+                            }}>
+                                CONTACT
+                            </Typography>
+                        </Box>                
+                </Grid>
+                <Grid item xs={18} sm={18} md={6} lg={6} xl={6}>
+                    <Box sx={{
+                            display:'flex',
+                            justifyContent:'center'
+                        }}>
+                            <Typography variant="h4" sx={{
+                                mb:4,
+                                mt:8
+                            }}>
+                                RESOURCES
+                            </Typography>
+                        </Box>                  
+                </Grid>
+            </Grid>
         </Container>
     </Box>
     )
