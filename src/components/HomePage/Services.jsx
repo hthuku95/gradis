41046ae2f580d61f8957eabaa6929d3094a6f1ca
HARRY OMOLLOH <hthuku95@gmail.com>
2023-08-12 00:11:57 +0300

    Added  message bar

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -2,7 +2,27 @@ import { Box,Container,Typography,Grid,Button } from "@mui/material";
 
 const Services = () => {
     return (
-        <Container id='section3'>
+        <Box id='section3' sx={{
+            mb:20
+        }}>
+            <Box sx={{
+                display:'flex',
+                justifyContent:'center',
+                textAlign:'center',
+                backgroundColor:'custom.second',
+                color:'custom.white',
+                mb:4,
+                pt:4,
+                pb:4
+            }}>
+                <Container>
+                    <Typography variant='body1' sx={{
+                        color:'custom.white'
+                    }}>
+                        Gradis is a customer-centric organisation commited to providing a first-class customer experience. We provide our services on taking time to listen to and understand our clients from the very begining. 
+                    </Typography>
+                </Container>
+            </Box>
             <Box sx={{
                 justifyContent:'center',
                 textAlign:'center',
@@ -11,21 +31,17 @@ const Services = () => {
                 <Typography variant="h2" sx={{ 
                     textAlign:'center',
                     color: 'custom.third',
-                    mb: 2,
+                    mb: 4,
                     mt: 2,
                     borderBottom: `2px solid custom.third`,
                 }}>
                     Services
                 </Typography>
-                <Typography variant="body1" sx={{
-                    color: 'custom.second',
-                    textAlign: 'center',
-                    width: '75%',
-                    margin: '0 auto',
-                    mb: 4 
-                }}>
-                    As a passionate team of experienced data scientists, analysts, and engineers, we strive to turn your data into clear decisive actionable insights, patnering to supporting your organisational goals, missions and objectives. 
-                </Typography>
+            </Box>
+            <Box sx={{
+                mb:6
+            }}>
+            <Container>
                 <Box sx={{
                     mb:6
                 }}>
@@ -120,21 +136,11 @@ const Services = () => {
                                 Robotic Process Automation
                             </Button>
                         </Grid>
-                    </Grid>
+                    </Grid>  
                 </Box>
-                <Box sx={{
-                    display:'flex',
-                    justifyContent:'center',
-                    mb:6
-                }}>
-                    <Typography variant='body1' sx={{
-                        color:'custom.third'
-                    }}>
-                        Gradis is a customer-centric organisation commited to providing a first-class customer experience. We provide our services on taking time to listen to and understand our clients from the very begining. 
-                    </Typography>
-                </Box>
+            </Container>
             </Box>
-        </Container>
+        </Box>
     )
 }
 
